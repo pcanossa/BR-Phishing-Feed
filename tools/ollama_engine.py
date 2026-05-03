5bde77773ce86cd1aa9aa48a6ab19e428967731c
Patrícia Canossa <p.canossa@hotmail.com>
2026-05-03 18:07:26 -0300

    Auto-update feed: 2026-05-03 18:07:24

--- tools/ollama_engine.py
+++ tools/ollama_engine.py
@@ -12,7 +12,7 @@ def ollama_engine(message):
 
     try:
             full_response = []
-            for part in client.chat('gpt-oss:120b-cloud', messages=message, stream=True):
+            for part in client.chat('llama3.2:latest', messages=message, stream=True):
               content = part['message']['content']
               full_response.append(content)
             return "".join(full_response)
